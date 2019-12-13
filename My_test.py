f31af93814a0001708d6a7ecffd658c6576dd9e4
Muttsu <22158218+Muttsu@users.noreply.github.com>
2019-12-12 23:19:04 -0500

    asdf

--- My_test.py
+++ My_test.py
@@ -4,13 +4,10 @@ data = Abalation_Study.ThreeExperiement_DataLoading()
 """
 We choose alpha to test and validate the result from paper
 """
-Abalation_Study.test_experiment(data= data,alphas=20)
+Abalation_Study.test_experiment(data, alphas=20)
 
 """
 We choose different feature size to test and validate the result from paper
 We test especially when feature sizes goes large here. Something interesting happen.
 """
-Abalation_Study.test_synthetic_features(data_size = 10000, feature_size =[9,10,11],folds=3, alphas=10, range=100,solver='lasso')
-
-
-
+Abalation_Study.test_synthetic_features(data_size = 10000, feature_size =[9,10,11],folds=3, alphas=5, range=100,solver='lasso')
---
+++ ablation.py
@@ -0,0 +1,107 @@
+import csv
+import time
+import numpy as np
+import pandas as pd
+import Booster as bst
+
+from sklearn.base import TransformerMixin
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
+from sklearn.model_selection import GridSearchCV
+
+def get_new_clf(solver):
+    if solver == "linear":
+        return LinearRegression(fit_intercept=False)
+    elif solver == "lasso":
+        return Lasso(fit_intercept=False)
+    elif solver == "ridge":
+        return Ridge(fit_intercept=False)
+    if solver == "elastic":
+        return ElasticNet(fit_intercept=False)
+
+def test(data,labels,weights,solver,folds,alphas):
+    clf = bst.get_new_clf(solver, folds=folds, alphas=alphas)
+    time_coreset, clf_coreset = bst.coreset_train_model(data, labels, weights, clf, folds=folds, solver=solver)
+    score_coreset = bst.test_model(data, labels, weights, clf)
+
+    clf = bst.get_new_clf(solver, folds=folds, alphas=alphas)
+    time_real, clf_real = bst.train_model(data, labels, weights,clf)
+    score_real = bst.test_model(data, labels, weights, clf)
+
+    print(
+        f'solver: {solver}\n'
+        f'alphas: {alphas},\n'
+        f'original_time = {time_real}\n'
+        f'coreset_time = {time_coreset}\n'
+        f'score_diff = {np.abs(score_coreset - score_real)}\n'
+        f'coef_diff = {np.sum(np.abs(clf_real.coef_ - clf_coreset.coef_))}\n'
+        )
+
+def load_datasets():
+
+    ds1 = pd.read_csv(
+        "3D_spatial_network.csv",
+        header=None,
+    ).iloc[:,1:].to_numpy()
+
+    ds2 = pd.read_csv(
+        "household_power_consumption.csv",
+        sep=';',
+        na_values='?'
+    ).iloc[:,2:5].dropna().to_numpy()
+
+    ds3 = pd.read_csv(
+        "kc_house_data.csv",
+        index_col=0
+    ).iloc[:, [2, 4, 5, 6, 7, 11, 12, 13, 1]].to_numpy().astype(np.int)
+
+    return (ds1,ds2,ds3)
+
+def test_real(datasets,folds=3,alphas=100):
+
+    selected_solver = ["lasso", "ridge", "elastic"]
+    count = 0
+
+    for i, ds in enumerate(datasets):
+        print(f'Test result of dataset{i}\n')
+        for solver in selected_solver:
+            X = ds[:, :-1]
+            y = ds[:, -1].reshape(ds.shape[0], 1)
+            w = np.ones(ds.shape[0])
+
+            test(X,y,w,solver,folds,alphas)
+
+def test_synthetic(data_size = 10000, feature_size =[3,5,7], folds=3,alphas=100,range=100,solver='lasso'):
+    n = data_size
+    d = feature_size
+    weights = np.ones(n)
+    labels = np.floor(np.random.rand(n, 1) * range)
+
+    for i in feature_size:
+        print(f'\nTest result of d={i}')
+        data = np.floor(np.random.rand(n, i) * range)
+        test(data,labels,weights,solver,folds,alphas)
+
+
+        # #########RIDGE REGRESSION#############
+        # clf = Booster.get_new_clf(solver, folds=folds, alphas=num_of_alphas)
+        # time_coreset, clf_coreset = Booster.coreset_train_model(data, labels, weights, clf, folds=folds, solver=solver)
+        # score_coreset = Booster.test_model(data, labels, weights, clf)
+        #
+        # clf = Booster.get_new_clf(solver, folds=folds, alphas=num_of_alphas)
+        # time_real, clf_real = Booster.train_model(data, labels, weights, clf)
+        # score_real = Booster.test_model(data, labels, weights, clf)
+        #
+        # print(
+        #     " solver: {}\n number_of_alphas: {}, \nscore_diff = {}\n---->coef diff = {}\n---->coreset_time = {}\n---->data time = {}".format(
+        #         solver,
+        #         num_of_alphas,
+        #         np.abs(score_coreset - score_real),
+        #         np.sum(np.abs(clf_real.coef_ - clf_coreset.coef_)),
+        #         time_coreset,
+        #         time_real))
+
+# def test_synthetic_size(num_of_data=10000, num_of_feature=3,sol='lasso',folds=3,alphas=100,range=100):
+
+if __name__ == '__main__':
+    datasets = load_datasets();
+    test_real(datasets, alphas=100)
