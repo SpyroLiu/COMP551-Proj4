2be4c278989ea0409918ecc839d41946800a61e7
Muttsu <22158218+Muttsu@users.noreply.github.com>
2019-12-13 23:48:51 -0500

    asdf'

--- coreset.py
+++ coreset.py
@@ -1,12 +1,11 @@
 """
 """
 
-
 import numpy as np
 import scipy.linalg as linalg
 
 
-def caratheodory(P, u=None, tol=1e-8, dtype=np.float):
+def caratheodory(P, u=None, tol=1e-8, dtype=np.float32):
     (n, d) = P.shape
 
     if u is None:
@@ -25,7 +24,7 @@ def caratheodory(P, u=None, tol=1e-8, dtype=np.float):
 
         positive = v > 0
         u[mask] = u[mask] - np.min(u[mask][positive]/v[positive]) * v
-        u[np.isclose(u,0,atol=tol)] = 0
+        u[np.isclose(u, 0, atol=tol)] = 0
         # unrolled recursive step
         mask = u != 0
         n = np.sum(mask)
@@ -33,7 +32,7 @@ def caratheodory(P, u=None, tol=1e-8, dtype=np.float):
     return u
 
 
-def fast_caratheodory(P, u=None, k = None, tol=1e-8, dtype=np.float):
+def fast_caratheodory(P, u=None, k = None, size=None, tol=1e-8, dtype=np.float32):
     n, d = P.shape
     n_ = n # save original size
 
@@ -43,46 +42,49 @@ def fast_caratheodory(P, u=None, k = None, tol=1e-8, dtype=np.float):
         u = u.astype(dtype)
 
     mask = u != 0
+    P = P[mask]
+    u = u[mask]
+    idx = np.arange(n_, dtype=np.int)[mask] # idx of the computed weights
+
     n = np.sum(mask)
 
     # default fastest value for k
     if not k:
         k = 2 *d + 2
+    k_ = k
 
+    # scaling, see original implementation
     u_sum = np.sum(u)
     u /= u_sum
 
-    # idx of the computed weights
-    idx = np.arange(n, dtype=np.int)
-
-    while n > d + 1:
+    if not size:
+        size = d + 1
+    while n > size:
         # discretize cluster count and size
-        cluster_size = int(np.ceil(n/k))
+        cluster_size = int(np.ceil(n/k_))
         k = int(np.ceil(n/cluster_size))
         # fill data to match cluster size
         fill = cluster_size - n % cluster_size
         if fill != cluster_size:
             P = np.append(P, np.zeros((fill, d), dtype))
             u = np.append(u, np.zeros(fill, dtype))
-            idx = np.append(idx, np.zeros(fill, dtype=np.int))
-
+            idx = np.append(idx, np.zeros(fill, dtype=np.int32))
+        # partition into clusters
         clusters = P.reshape(k, cluster_size, d)
         cluster_weights = u.reshape(k, cluster_size)
         cluster_idx = idx.reshape(k, cluster_size)
 
-        # means of the clusters
+        # weighted means of the clusters
         means = np.einsum('ijk,ij->ik', clusters, cluster_weights)
-
-        w = caratheodory(means, np.ones(k, dtype)/cluster_size, tol, dtype)
+        # call to caratheodory using weighted cluster means
+        w = caratheodory(means, np.ones(k, dtype), tol, dtype)
 
         cluster_mask = w != 0
-
-        n = np.sum(cluster_mask) * cluster_size
-
         P = clusters[cluster_mask].reshape(-1,d)
-        u = (k * cluster_weights[cluster_mask] * w[cluster_mask][:, np.newaxis]).reshape(-1)
+        u = (cluster_weights[cluster_mask] * w[cluster_mask][:, np.newaxis]).reshape(-1)
         idx = cluster_idx[cluster_mask].reshape(-1)
 
+        n = u.shape[0]
 
     u_new = np.zeros(n_, dtype)
     u_new[idx] = u
@@ -90,15 +92,25 @@ def fast_caratheodory(P, u=None, k = None, tol=1e-8, dtype=np.float):
     return u_new
 
 
-def coreset(A, k = None):
-    # TODO
-    pass
+def coreset(X, y=None, weights=None, k=None, size=None, tol=1e-8, dtype=np.float32):
+    n, d = X.shape
+    P = X[:,:,np.newaxis]
+    P = np.einsum('ikj,ijk->ijk', P, P)
+    P = P.reshape(n,d**2)
+
+    w = np.sqrt(fast_caratheodory(P, weights, k, size, tol, dtype))
+    idx_mask = w != 0
+
+    if y is not None:
+        return X[idx_mask], y[idx_mask], w[idx_mask][:,np.newaxis]
+    else:
+        return X[idx_mask], w[idx_mask][:,np.newaxis]
 
 
 
 if __name__ == '__main__':
     from ablation import load_datasets
-    from Booster import Caratheodory
+    from Booster import Caratheodory, Fast_Caratheodory
     d = load_datasets()[0][:100]
     u = np.arange(100)
 
@@ -108,5 +120,13 @@ if __name__ == '__main__':
     assert(np.allclose(d.T @ c, d.T @ u))
 
     # test fast_caratheodory
-    f = fast_caratheodory(d)
-    print(f)
+    c = fast_caratheodory(d,u)
+    assert(np.all(u == np.arange(100)))
+    assert(np.allclose(d.T @ c, d.T @ u))
+
+    A = d[:,:-1]
+    y = d[:,-1]
+    C, y, w = coreset(A, y)
+    S = w * C
+    print((A.T @ A, S.T @ S))
+    assert(np.allclose(A.T @ A, S.T @ S))
